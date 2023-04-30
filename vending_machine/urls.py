5b1fc9c93d064f5c4b5c8302dc9b648af095625c
dxZhang <dxzhang49@gmail.com>
2023-04-30 20:11:08 +0800

    Add Purchase and Purchase History

--- vending_machine/urls.py
+++ vending_machine/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'vendingmachine'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('purchase/<item_id>/', views.purchase, name='purchase'),
     path('add/', views.top_up, name='add'),
     path('flush', views.flush, name='flush'),
+    path('purchase/', views.purchase, name='purchase'),
+    path('history/', views.history, name='history'),
 ]
